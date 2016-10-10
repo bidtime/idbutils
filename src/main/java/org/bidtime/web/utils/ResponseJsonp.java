3da2d512db3a68724b957cc4fd6581f64c47e87f
bidtime <bidtime@126.com>
2016-10-10 11:36:04 +0800

    responseJsonp

--- src/main/java/org/bidtime/web/utils/ResponseJsonp.java
+++ src/main/java/org/bidtime/web/utils/ResponseJsonp.java
@@ -1,5 +1,6 @@
 package org.bidtime.web.utils;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -31,15 +32,27 @@ public class ResponseJsonp {
 //		}
 //	}
 	
+	public static void setCookies(HttpServletRequest request, HttpServletResponse response) {
+		Cookie[] cs = request.getCookies();
+		if (cs != null) {
+			for (Cookie c : cs) {
+				response.addCookie(c);
+			}
+		}
+	}
+	
 	public static void writeString(HttpServletRequest request, HttpServletResponse response, 
 			String msg) {
 		String callback_name = RequestUtils.getString(request, CALLBACK_FUN);
-		if (StringUtils.isNotEmpty(callback_name)) {		
+		if (StringUtils.isNotEmpty(callback_name)) {
+			request.getCookies();
 			StringBuilder sb = new StringBuilder();
 			sb.append(callback_name);
 			sb.append("(");
 			sb.append(msg);
 			sb.append(")");
+			setCookies(request, response);
+			ResponseUtils.writeString(sb.toString(), response);
 		} else {
 			ResponseUtils.writeString(msg, response);
 		}
